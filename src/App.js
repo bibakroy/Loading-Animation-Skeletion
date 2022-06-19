cab548d3d6a4b648703c23192562fd0cb6fd6626
Bibak <bibak@pop-os.localdomain>
2022-06-19 14:22:53 +0600

    create skeletion of a loading animation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import Card from "./Card";
+import Skeleton from "./Skeleton";
 
-function App() {
+export default function App() {
+  const [posts, setPosts] = useState([]);
+  const [isEmpty, setIsEmpty] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetch("https://jsonplaceholder.typicode.com/posts")
+        .then((response) => response.json())
+        .then((data) => {
+          setPosts(data);
+          setIsEmpty(false);
+        })
+        .catch((err) => console.log(err));
+    }, 3000);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Posts</h1>
+      {
+        // rendering 4 skeleton cards when posts array is empty
+        isEmpty && [1, 2, 3, 4].map((value) => <Skeleton key={value} />)
+      }
+      {
+        //rendering actual cards when posts array is populated with some data
+        isEmpty ||
+          posts.map((post) => (
+            <Card key={post.id} title={post.title} body={post.body} />
+          ))
+      }
+    </>
   );
 }
-
-export default App;
